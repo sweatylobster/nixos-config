098b754d7edca847012b493117a78df678773fec
reggie-tertullian <max@melissa.com>
2024-09-16 02:26:38 -0700

    set up minimal zsh config

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -4,11 +4,38 @@
     enable = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
-    # histSize = 10000;
+
+    history = {};
+
     shellAliases = {
+
+      # indecisive
       e = "nvim";
       n = "nvim";
       v = "nvim";
+
+      lg = "lazygit";
+
+      # who needs ls, anyway?
+      ls = "eza";
+      ll = "eza --long --header --git --icons=always --all";
+      tree = "eza --tree";
+
+      texclean = "fd -e log -e aux -x rm {}";
+
+      t = "tmux";
     };
+
+    # initExtra = builtins.readFile ./config.zsh;
+    initExtra = ''
+      function y() {
+        local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+        yazi "$@" --cwd-file="$tmp"
+        if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+          cd -- "$cwd"
+        fi
+        rm -f -- "$tmp"
+      }
+    '';
   };
 }
