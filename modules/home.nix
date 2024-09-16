082218a8bca4c65bd2cd74eb1b1f3de8e011614e
reggie-tertullian <max@melissa.com>
2024-09-16 02:26:01 -0700

    clean up home.nix

--- modules/home.nix
+++ modules/home.nix
@@ -5,27 +5,3 @@
   programs.home-manager.enable = true;
   programs.nix-index.enable = true;
 }
-
-
-# home-manager.users.cowmaster = { pkgs, ... }: {
-#   home.packages = with pkgs; [
-#     httpie 
-#     yazi 
-#   ];
-#
-#   programs = {
-#     # Use zsh :)
-#     zsh = {
-#       enable = true;
-#       enableCompletion = true;
-#       syntaxHighlighting = {
-#         enable = true;
-#       };
-#     };
-#     # Use starship!
-#     starship.enable = true;
-#   };
-#
-#   home.stateVersion = "24.05";
-# };
-
