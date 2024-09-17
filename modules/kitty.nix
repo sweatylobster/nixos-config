449fd1aae45a30a6e3a6c8bf1f3ae04881178c14
reggie-tertullian <max@melissa.com>
2024-09-16 22:11:25 -0700

    fix kitty aesthetics

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -4,11 +4,16 @@
     enable = true;
 	environment = { works = "yes"; };
 	font = {
-	  name = "FiraCode";
-	  size = 18;
+	  name = "MartianMono Nerd Font Mono";
+	  size = 12;
 	};
 	keybindings = {};
 	shellIntegration.enableZshIntegration = true;
+	settings = {
+	  enable_audio_bell = false;
+	  hide_window_decorations = true;
+	  background_opacity = "0.84";
+	};
 	theme = "Tokyo Night"; # "tokyo_night_night";
   };
 }
