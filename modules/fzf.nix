f71dde1fa00bde91f09bd309527008f4291c0aae
reggie-tertullian <max@melissa.com>
2024-09-17 00:37:44 -0700

    add fzf colors

--- modules/fzf.nix
+++ modules/fzf.nix
@@ -3,5 +3,23 @@
     enable = true;
     enableZshIntegration = true;
     enableBashIntegration = true;
+    colors = {
+      "bg" = "#16161e";
+      "bg+" = "#283457";
+      "border" = "#27a1b9";
+      "fg" = "#c0caf5";
+      "gutter" = "#16161e";
+      "header" = "#ff9e64";
+      "hl" = "#2ac3de";
+      "hl+" = "#2ac3de";
+      "info" = "#545c7e";
+      "marker" = "#ff007c";
+      "pointer" = "#ff007c";
+      "prompt" = "#2ac3de";
+      "query" = "#c0caf5:regular";
+      "scrollbar" = "#27a1b9";
+      "separator" = "#ff9e64";
+      "spinner" = "#ff007c";
+    };
   };
 }
