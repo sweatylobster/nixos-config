3710a98cb6a1e52b697f3fd8c94df947af94e59d
reggie-tertullian <max@melissa.com>
2024-09-19 01:20:28 -0700

    fixup ssh config

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -1,5 +1,34 @@
 { config, ... }: {
   programs.ssh = {
     enable = true;
+    includes = [ "~/.ssh/config.local" ];
+    serverAliveInterval = 60;
+    matchBlocks = {
+      # "*" = {
+      #   identityFile = "~/.ssh/id_ed25519";
+      #   setEnv = {
+      #     TERM = "xterm-256color";
+      #   };
+      #
+      #   sendEnv = [
+      #     "COLORTERM"
+      #   ];
+      #
+      #   # extraOptions = {
+      #   #   IgnoreUnknown = "UseKeychain";
+      #   #   UseKeychain = "yes";
+      #   # };
+      # };
+      "github.com" = {
+        serverAliveInterval = 0;
+        extraOptions = {
+          ControlMaster = "auto";
+          ControlPath = "~/.ssh/github.sock";
+          ControlPersist = "5m";
+        };
+      };
+    };
   };
+
+  home.file.".ssh/rc".source = config.lib.file.mkOutOfStoreSymlink ./rc;
 }
