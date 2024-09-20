42e04477d051e429354c4f0908c181e74227296c
reggie-tertullian <max@melissa.com>
2024-09-20 00:01:34 -0700

    set $PROJECTS dir as ~/code

--- modules/home.nix
+++ modules/home.nix
@@ -2,6 +2,19 @@
   home.username = "cowmaster";
   home.stateVersion = "24.05";
 
+  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   programs.nix-index.enable = true;
+
+  home.sessionVariables = {
+    LC_ALL = "en_US.UTF-8";
+    LC_CTYPE = "en_US.UTF-8";
+    PROJECTS = "$HOME/code";
+  };
+
+  # Ensure ~/code -- ~/Developer for caarlos0 -- exists.
+  # assumed for other activations, especially on darwin.
+  home.activation.developer = ''
+    mkdir -p ~/code
+  '';
 }
