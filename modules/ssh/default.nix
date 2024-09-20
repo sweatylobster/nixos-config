aa780b9a051ee4653b0dd46b1b4c8d2902162ada
reggie-tertullian <max@melissa.com>
2024-09-20 00:00:15 -0700

    define useful bins

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -4,21 +4,48 @@
     includes = [ "~/.ssh/config.local" ];
     serverAliveInterval = 60;
     matchBlocks = {
-      # "*" = {
-      #   identityFile = "~/.ssh/id_ed25519";
-      #   setEnv = {
-      #     TERM = "xterm-256color";
-      #   };
-      #
-      #   sendEnv = [
-      #     "COLORTERM"
-      #   ];
-      #
-      #   # extraOptions = {
-      #   #   IgnoreUnknown = "UseKeychain";
-      #   #   UseKeychain = "yes";
-      #   # };
-      # };
+      "*" = {
+        identityFile = "~/.ssh/id_ed25519";
+        setEnv = {
+          TERM = "xterm-256color";
+        };
+
+        sendEnv = [
+          "COLORTERM"
+        ];
+
+        extraOptions = {
+          IgnoreUnknown = "UseKeychain";
+          UseKeychain = "yes";
+        };
+      };
+      "dev" = {
+        forwardAgent = true;
+        remoteForwards = [
+          {
+            # pbcopy
+            bind.port = 2224;
+            host.address = "127.0.0.1";
+            host.port = 2224;
+          }
+          {
+            # pbpaste
+            bind.port = 2225;
+            host.address = "127.0.0.1";
+            host.port = 2225;
+          }
+          {
+            # xdg-open-svc
+            bind.port = 2226;
+            host.address = "127.0.0.1";
+            host.port = 2226;
+          }
+        ];
+        extraOptions = {
+          RequestTTY = "true";
+          RemoteCommand = "tmux new -A -s default";
+        };
+      };
       "github.com" = {
         serverAliveInterval = 0;
         extraOptions = {
