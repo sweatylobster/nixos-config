f4e73691f869bcb6d7564be8276c666ba128d56d
Max de Hoyos <max.dehoyos@gmail.com>
2024-09-21 11:42:38 -0700

    update darwin.nix
    Set some defaults.
    Seems like we can mess with "com.amethyst.Amethyst", but I'm gonna wait
    on that.

--- machines/shared/darwin.nix
+++ machines/shared/darwin.nix
@@ -7,7 +7,7 @@
   nix.settings.trusted-users = [ "root" "max" ];
   system.stateVersion = 5; # TODO: what is this, mitchell?
 
-  # ids.uids.nixbld = 300; # TODO: Trying to outrun fate of MacOS 15
+  # ids.uids.nixbld = 300; # NOTE: Can do this to outrun fate of MacOS 15
 
   users.users.max = {
     name = "max";
@@ -17,14 +17,18 @@
   homebrew = {
     enable = true;
     casks = [
-      # "deckset"       # might buy; .md -> presentation slides.
+      # "deckset" # might buy; .md -> presentation slides.
       "discord"
       # "font-jetbrains-mono"
       # "font-jetbrains-mono-nerd-font"
       "hammerspoon"
-      # "monodraw"      # might buy; ASCII diagrams.
-      # "soulver"       # might buy; text calculator for dates, measurements, etc.
+      # "monodraw" # might buy; ASCII diagrams.
+      # "soulver" # might buy; text calculator for dates, measurements, etc.
+      "raycast"
       "whatsapp"
+    ];
+    taps = [
+      "rgcr/m-cli" # expose options for use in `CustomUserPreferences`
     ];
     # NOTE: Today's the only cool one.
     masApps = {
@@ -32,8 +36,16 @@
     };
   };
 
+  # TODO: Figure out what you want
+  # fonts.packages = with pkgs; [
+  #   (nerdfonts.override { fonts = [ "FiraMono" ]; })
+  # ];
+
   system = {
+
     defaults = {
+      menuExtraClock.show24Hour = true;
+
       dock = {
         autohide = true;
         autohide-delay = 0.0;
@@ -52,16 +64,30 @@
           # "/Applications/Todoist.app"         # don't have
           # "/System/Applications/Notes.app"    # just use nvim
           "/System/Applications/Calendar.app"
-          "/System/Applications/Firefox.app" # NOTE: my addition
+          "/Applications/Firefox.app" # NOTE: my addition
           # "/System/Cryptexes/App/System/Applications/Safari.app"
           # "/Applications/Ghostty.app"         # don't have
           "/System/Applications/Mail.app"
           "/System/Applications/Messages.app"
           "/Applications/Discord.app"
-          # "/System/Applications/Music.app"    # wtf
-          "/System/Applications/Spotify.app"
+          "/Applications/Spotify.app"
         ];
       };
+
+      finder = {
+        FXPreferredViewStyle = "Nlsv"; # [ "icnv" "clmv" "Flwv" ];
+        _FXShowPosixPathInTitle = true;
+        FXEnableExtensionChangeWarning = false;
+        AppleShowAllFiles = true;
+        ShowStatusBar = true;
+        ShowPathbar = true;
+      };
+
+      loginwindow = {
+        GuestEnabled = false; # disable guest user
+        SHOWFULLNAME = true; # show full name in login window
+      };
+
       NSGlobalDomain = {
         ApplePressAndHoldEnabled = false;
         KeyRepeat = 2;
@@ -74,19 +100,19 @@
         NSAutomaticQuoteSubstitutionEnabled = false;
         NSAutomaticSpellingCorrectionEnabled = false;
         AppleInterfaceStyle = "Dark";
-        NSDocumentSaveNewDocumentsToCloud = false; # TODO: Do I want this?
+        NSDocumentSaveNewDocumentsToCloud = false; # disabled by default.
         _HIHideMenuBar = false;
         "com.apple.springing.delay" = 0.0;
+        "com.apple.trackpad.forceClick" = 1;
       };
-      finder = {
-        FXPreferredViewStyle = "Nlsv"; # TODO: What the hell
-        _FXShowPosixPathInTitle = true;
-        FXEnableExtensionChangeWarning = false;
-        AppleShowAllFiles = true;
-        ShowStatusBar = true;
-        ShowPathbar = true;
-      };
+
+      # rgcr/m-cli
+      # defaults: `defaults read | bat` (it's really long)
       CustomUserPreferences = {
+        "com.apple.AdLib" = {
+          allowApplePersonalizedAdvertising = false;
+          # allowIdentifierForAdvertising = false;
+        };
         "com.apple.NetworkBrowser" = { BrowseAllInterfaces = true; };
         "com.apple.screensaver" = {
           askForPassword = true;
@@ -95,19 +121,22 @@
         "com.apple.trackpad" = { scaling = 2; }; # TODO: Not sure about these
         "com.apple.mouse" = { scaling = 2.5; };
         "com.apple.desktopservices" = { DSDontWriteNetworkStores = false; };
-        "com.apple.LaunchServices" = { LSQuarantine = true; }; # TODO: What does this mean
+        "com.apple.LaunchServices" = { LSQuarantine = true; }; # TODO: Default, but not sure what it means
         "com.apple.finder" = {
-          ShowExternalHardDrivesOnDesktop = false;
-          ShowRemovableMediaOnDesktop = false;
+          ShowExternalHardDrivesOnDesktop = true;
+          ShowRemovableMediaOnDesktop = true;
           WarnOnEmptyTrash = false;
+          _FXSortFoldersFirst = true;
+          # When performing a search, search the current folder by default
+          FXDefaultSearchScope = "SCcf";
         };
         "NSGlobalDomain" = {
           NSNavPanelExpandedStateForSaveMode = true;
           NSTableViewDefaultSizeMode = 1;
           WebKitDeveloperExtras = true;
         };
-        "com.apple.ImageCapture" = { "disableHotPlug" = true; }; # TODO: What
-        # NOTE: I don't use Mail; maybe should.
+        # Prevent Photos from opening automatically when devices are plugged in
+        "com.apple.ImageCapture" = { "disableHotPlug" = true; };
         "com.apple.mail" = {
           DisableReplyAnimations = true;
           DisableSendAnimations = true;
@@ -148,7 +177,31 @@
           ShowPrivacyReportInFavorites = 1;
           ShowRecentlyClosedTabsPreferenceKey = 1;
         };
+        # NOTE: Seems like we can configure other apps here, as well.
+        # "com.amethyst.Amethyst" = {
+        #   SUAutomaticallyUpdate = 0;
+        #   SUEnableAutomaticChecks = 0;
+        #   "mouse-follows-focus" = 0;
+        #   "new-windows-to-main" = 0;
+        #   "screen-padding-bottom" = 15;
+        #   "screen-padding-left" = 15;
+        #   "screen-padding-right" = 15;
+        #   "screen-padding-top" = 15;
+        # };
       };
+
+      # Final default.
+      SoftwareUpdate.AutomaticallyInstallMacOSUpdates = false;
     };
+    ### defaults = {...}; end
+
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToEscape = true;
+    };
+
   };
+
+  # Add ability to used TouchID for sudo authentication
+  security.pam.enableSudoTouchIdAuth = true;
 }
