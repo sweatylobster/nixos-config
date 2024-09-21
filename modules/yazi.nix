44908792d220faa3c0b793dc2a9e0bbf1213e484
reggie-tertullian <max@melissa.com>
2024-09-21 08:29:12 -0700

    chore: config yazi.nix a bit

--- modules/yazi.nix
+++ modules/yazi.nix
@@ -1,19 +1,41 @@
 { ... }: {
   programs.yazi = {
     enable = true;
-    # settings = {
-    #   yazi = "";   # TOML value
-    #   theme = "";  # TOML value
-    #   keymap = ''
-    #   '';          # TOML value
-    # };
-    # plugins = { # attribute set of (path or package)
-    #   foo = ./foo;
-    #   bar = pkgs.bar
-    # };
-    # flavor = {  # attribute set of (path or package)
-    #   myflavor = ./myflavor;
-    # };
+    settings = {
+      log = {
+        enabled = false;
+      };
+      manager = {
+        ratio = [ 1 4 3 ];
+        sort_by = "natural";
+        sort_dir_first = true;
+        show_hidden = false;
+        show_symlink = true;
+      };
+      preview = {
+        wrap = "no";
+        # tab_size = "";
+        # max_width = "";
+        # max_height = "";
+        image_delay = 40; # don't immediately render.
+        image_filter = "nearest"; # [ nearest triangle catmull-rom gaussian lanczos3 ]
+        image_quality = 50; # 50-90
+      };
+      open = { };
+    };
+    # yazi = ""; # TOML value
+    # theme = ""; # TOML value
+    # keymap = ''
+    # ''; # TOML value
+    plugins = {
+      # attribute set of (path or package)
+      # foo = ./foo;
+      # bar = pkgs.bar
+    };
+    flavors = {
+      # attribute set of (path or package)
+      # myflavor = ./myflavor;
+    };
     # initLua = ./init.lua # null or path, default's null
   };
 }
