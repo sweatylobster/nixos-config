a1d276ffbee3be3339f2c31fe49844a4db983f40
Max de Hoyos <max.dehoyos@gmail.com>
2024-09-24 16:25:44 -0700

    chore(alacritty): change font options

--- modules/alacritty.nix
+++ modules/alacritty.nix
@@ -10,13 +10,17 @@
       # [ DejaVuSansM, FiraMono, Hack, Hasklug, Lilex, MartianMono (size=12), UbuntoMono (size=18),  ]
       font = {
         size = if pkgs.stdenv.isLinux then 12 else 16;
-        bold.family = "MartianMono Nerd Font Mono";
-        italic.family = "MartianMono Nerd Font Mono";
-        normal.family = "MartianMono Nerd Font Mono";
-        bold_italic.family = "MartianMono Nerd Font Mono";
+        normal = {
+          family = "MartianMono Nerd Font Mono";
+          style = "Regular";
+        };
+        # bold.family = "MartianMono Nerd Font Mono";
+        # italic.family = "MartianMono Nerd Font Mono";
+        # bold_italic.family = "MartianMono Nerd Font Mono";
       };
       window = {
         decorations = "None";
+        blur = false;
         opacity = 0.84;
         option_as_alt = "Both";
       };
