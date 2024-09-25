f66253b7c2051d949d14d644b7d4be59c2b66bb3
Max de Hoyos <max.dehoyos@gmail.com>
2024-09-25 12:16:26 -0700

    feat(kitty|alacritty): Use left option as alt.
    This preserves the ability to type special characters with right alt,
    which I might learn to do.

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -13,6 +13,7 @@
       enable_audio_bell = false;
       hide_window_decorations = true;
       background_opacity = "0.84";
+      macos_option_as_alt = "left";
     };
     themeFile = "tokyo_night_night"; # "Tokyo Night";
   };
