9faeb4600f721287fa91a1a8808cab2c3f60e8c3
Max de Hoyos <max.dehoyos@gmail.com>
2024-09-27 16:55:10 -0700

    feat(tmux): remove extrakto, add yank, add trap

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -5,13 +5,24 @@
     terminal = "screen-256color";
     clock24 = true;
     plugins = with pkgs.tmuxPlugins; [
-      # better-mouse-mode
-      # copy-toolkit
-      extrakto
-      catppuccin
       tmux-thumbs
       sensible
+      yank
     ];
     extraConfig = builtins.readFile ./tmux.conf;
   };
+
+  programs.fish.interactiveShellInit = ''
+    # traps the shell quit and switch to the last session if it's the last pane
+    # if last session is not available anymore, switch to default
+    function __trap_exit_tmux
+        test (tmux list-windows | count) = 1 || exit
+        test (tmux list-panes | count) = 1 || exit
+        tmux switch-client -l || tmux switch-client -t default
+    end
+
+    if set -q TMUX
+        trap __trap_exit_tmux EXIT
+    end
+  '';
 }
