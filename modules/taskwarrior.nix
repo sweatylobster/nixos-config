c2ae1457fdbdc0fc105efb69f1e61e35cd1aad8a
Max de Hoyos <max.dehoyos@gmail.com>
2024-11-25 01:29:13 -0800

    feat(shell): taskwarrior theme and data location

--- modules/taskwarrior.nix
+++ modules/taskwarrior.nix
@@ -2,20 +2,19 @@
   programs.taskwarrior = {
     enable = true;
     package = pkgs.taskwarrior3;
+    dataLocation = "$HOME/.task";
+    colorTheme = "bubblegum-256"; # "dark-blue-256";
     extraConfig = ''
-      context.tf.read=project:tf
-      context.tf.write=project:tf
-      context.tfa.read=project:tfa
-      context.tfa.write=project:tfa
-      context.tfb.read=project:tfb
-      context.tfb.write=project:tfb
-      context.tfc.read=project:tfc
-      context.tfc.write=project:tfc
-      context.calls.read=project:calls
-      context.calls.write=project:calls
+      context.topflight.read=project:topflight
+      context.topflight.write=project:topflight
+      context.collections.read=project:collections
+      context.collections.write=project:collections
+      context.billing.read=project:billing
+      context.billing.write=project:billing
       context.refactor.read=project:refactor
       context.refactor.write=project:refactor
 
+      # Tiny Beelink (melissa=bee in Greek) NixOS machine
       context.melissa.read=project:melissa
       context.melissa.write=project:melissa
 
