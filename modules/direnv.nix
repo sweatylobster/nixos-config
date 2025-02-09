d5fe5d1b23fe25379cb98e5d502d425b79a59a78
Max de Hoyos <max.dehoyos@gmail.com>
2025-02-09 15:42:31 -0800

    feat(direnv): hide_env_diff = true;

--- modules/direnv.nix
+++ modules/direnv.nix
@@ -1,10 +1,13 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
     config = {
-      global = { load_dotenv = true; };
+      global = {
+        load_dotenv = true;
+        hide_env_diff = true;
+      };
     };
   };
 }
