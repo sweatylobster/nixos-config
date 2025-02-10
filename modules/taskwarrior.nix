e690403b67fa76e57c80157a49dd2d9ff5466683
Max de Hoyos <max.dehoyos@gmail.com>
2025-02-10 01:26:36 -0800

    feat(taskwarrior): fix contexts

--- modules/taskwarrior.nix
+++ modules/taskwarrior.nix
@@ -5,18 +5,12 @@
     dataLocation = "$HOME/.task";
     colorTheme = "bubblegum-256"; # "dark-blue-256";
     extraConfig = ''
-      context.topflight.read=project:topflight
+      context.topflight.read=project:topflight or project:aguila or +collections or +billing or +accounting or +aguila or +conexem or +medisoft
       context.topflight.write=project:topflight
-      context.collections.read=project:collections
-      context.collections.write=project:collections
-      context.billing.read=project:billing
-      context.billing.write=project:billing
-      context.refactor.read=project:refactor
-      context.refactor.write=project:refactor
-
-      # Tiny Beelink (melissa=bee in Greek) NixOS machine
-      context.melissa.read=project:melissa
-      context.melissa.write=project:melissa
+      context.medisoft.read=project:medisoft or +medisoft
+      context.medisoft.write=project:medisoft
+      context.personal.read=project:personal
+      context.personal.write=project:personal
     '';
   };
 }
