5a4bab3e7e5e68fe0b41166da9e8bf052e454619
Max de Hoyos <max.dehoyos@gmail.com>
2025-02-12 20:25:00 -0800

    chore(taskwarrior): simplify contexts

--- modules/taskwarrior.nix
+++ modules/taskwarrior.nix
@@ -5,10 +5,8 @@
     dataLocation = "$HOME/.task";
     colorTheme = "bubblegum-256"; # "dark-blue-256";
     extraConfig = ''
-      context.topflight.read=project:topflight or project:aguila or +collections or +billing or +accounting or +aguila or +conexem or +medisoft
+      context.topflight.read=project:topflight or +collections or +billing or +accounting or +aguila or +conexem or +medisoft
       context.topflight.write=project:topflight
-      context.medisoft.read=project:medisoft or +medisoft
-      context.medisoft.write=project:medisoft
       context.personal.read=project:personal
       context.personal.write=project:personal
     '';
