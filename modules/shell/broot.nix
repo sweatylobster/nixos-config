25b28d99532f9625130bb3223421425faabfed17
Max de Hoyos <max.dehoyos@gmail.com>
2025-04-19 23:21:55 -0700

    feat(broot): add some binds
    broot is really cool. more familiar with yazi, but i see where i *want*
    broot. image preview for PDFs might be nice, but yazi excels in that
    department. for yazi, i want to extend the typical 'z' (for fzf)
    binding, and write a previewer-command; that should mimic some of the
    qualities i like about broot, especially if i can truncate results from
    fd. it'd be interesting to just add broot as a file selector, for deeply
    nested files. smart-filter is already a great step in the right
    direction, but its inability to go backwards (with some keybinding) is
    not acceptable; might write my own plugin

--- modules/shell/broot.nix
+++ modules/shell/broot.nix
@@ -4,6 +4,13 @@
     enableFishIntegration = true;
     settings = {
       modal = false;
+      verbs = [
+        # these don't conflict with vim-tmux-navigator :)
+        { key = "alt-j"; internal = "line_down"; }
+        { key = "alt-k"; internal = "line_up"; }
+        # nifty
+        { invocation = "git-root"; shortcut = "gr"; internal = "focus {git-root}"; }
+      ];
     };
   };
 }
