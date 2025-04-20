534a69c46ee26389edf4e944ee6e290281c9e121
Max de Hoyos <max.dehoyos@gmail.com>
2025-04-20 03:46:33 -0700

    chore(neovim): remove go-related elements of nvim config

--- modules/editor/neovim/config/lua/user/lsp.lua
+++ modules/editor/neovim/config/lua/user/lsp.lua
@@ -23,46 +23,6 @@ end
 
 local lspconfig = require("lspconfig")
 require("lspconfig.ui.windows").default_options.border = "rounded"
-lspconfig.gopls.setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
-  settings = {
-    gopls = {
-      gofumpt = true,
-      codelenses = {
-        gc_details = true,
-        generate = true,
-        run_govulncheck = true,
-        test = true,
-        tidy = true,
-        upgrade_dependency = true,
-      },
-      hints = {
-        assignVariableTypes = true,
-        compositeLiteralFields = true,
-        compositeLiteralTypes = true,
-        constantValues = true,
-        functionTypeParameters = true,
-        parameterNames = true,
-        rangeVariableTypes = true,
-      },
-      analyses = {
-        nilness = true,
-        unusedparams = true,
-        unusedvariable = true,
-        unusedwrite = true,
-        useany = true,
-      },
-      staticcheck = true,
-      directoryFilters = { "-.git", "-node_modules" },
-      semanticTokens = true,
-    },
-  },
-  flags = {
-    debounce_text_changes = 150,
-  },
-})
-
 lspconfig.ts_ls.setup({
   capabilities = capabilities,
   on_attach = on_attach,
