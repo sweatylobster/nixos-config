5ab30353b13ae06a99f1819a739541a81c684ad8
Max de Hoyos <max.dehoyos@gmail.com>
2025-05-07 22:17:59 -0700

    feat(broot): add some navigation bindings, add pdf preview_transformer in docs

--- modules/shell/broot.nix
+++ modules/shell/broot.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.broot = {
     enable = true;
     enableFishIntegration = true;
@@ -6,17 +6,29 @@
       modal = true;
       initial_mode = "input";
       quit_on_last_cancel = false;
-      # default_flags = "-gh";
       special_paths = {
         "~/.config" = { "show" = "always"; };
+        # Paths to ignore.
+        "~/Applications" = { "show" = "never"; };
         "~/Library" = { "show" = "never"; };
         "~/go" = { "show" = "never"; };
-        "~/Applications" = { "show" = "never"; };
+        "~/nix-darwin" = { "show" = "never"; };
       };
       verbs = [
-        { invocation = "git-root"; shortcut = "gr"; internal = "focus {git-root}"; }
-        { invocation = "home"; key = "ctrl-h"; internal = ":focus ~"; }
-        { invocation = "code"; internal = ":focus ~/code"; }
+        # cd to commonly-used directories. Workaround the lack of zoxide.
+        { invocation = "git-root"; shortcut = "cdr"; internal = "focus {git-root}"; }
+        { invocation = "go-home"; shortcut = "cdh"; key = "ctrl-h"; internal = ":focus ~"; }
+        { invocation = "go-code"; shortcut = "cdc"; internal = ":focus ~/code"; }
+        { invocation = "go-aguila"; shortcut = "cda"; internal = ":focus ~/code/aguila"; }
+        { invocation = "go-nixos-config"; shortcut = "cdn"; internal = ":focus ~/nixos-config/"; }
+      ];
+      preview_transformers = [
+        {
+          input_extensions = [ "pdf" ];
+          output_extension = "png";
+          mode = "image";
+          command = [ "${pkgs.mupdf-headless}/bin/mutool" "draw" "-w" "1000" "-o" "{output-path}" "{input-path}" ];
+        }
       ];
     };
   };
