9aa0ccaa9fac04957931a21ede843eb655b9e9b1
Max de Hoyos <max.dehoyos@gmail.com>
2025-05-19 00:14:43 -0700

    feat(steam): use gamescope

--- machines/shared/steam.nix
+++ machines/shared/steam.nix
@@ -5,9 +5,14 @@
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
+    gamescopeSession.enable = true;
   };
 
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkgs) [ "steam" "steam-original" "steam-unwrapped" "steam-run" ];
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
 
   programs.gamemode.enable = true;
 }
