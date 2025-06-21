a415c8ec560c3d010dd7aa7699dd3add47d723f6
Max de Hoyos <max.dehoyos@gmail.com>
2025-06-21 15:21:49 -0700

    feat(keyd): add colemak-dh layout

--- machines/shared/keyd.nix
+++ machines/shared/keyd.nix
@@ -8,6 +8,39 @@
           main = {
             capslock = "esc";
           };
+          control = {
+            "1" = "setlayout(main)";
+            "2" = "setlayout(colemak-dh)";
+          };
+          "colemak-dh:layout" = {
+            a = "a";
+            b = "z";
+            c = "d";
+            d = "s";
+            e = "f";
+            f = "t";
+            g = "g";
+            h = "m";
+            i = "u";
+            j = "n";
+            k = "e";
+            l = "i";
+            m = "h";
+            n = "k";
+            o = "y";
+            p = ";";
+            q = "q";
+            r = "p";
+            s = "r";
+            t = "b";
+            u = "l";
+            v = "v";
+            w = "w";
+            x = "c";
+            y = "j";
+            z = "x";
+            ";" = "o";
+          };
         };
       };
     };
