680148e31150b23189ed8ae6182929074e08071f
Max de Hoyos <max.dehoyos@gmail.com>
2025-07-13 14:41:02 -0700

    feat(librewolf): add some nix bookmarks

--- modules/librewolf.nix
+++ modules/librewolf.nix
@@ -1,4 +1,5 @@
-{ pkgs, inputs, ... }: {
+{ pkgs, inputs, ... }:
+{
   # https://github.com/HirschBerge/Public-dots/blob/main/nixos/common/configs/firefox.nix
   programs.librewolf = {
     enable = true;
@@ -37,38 +38,66 @@
         force = true;
         engines = {
           "Nix Packages" = {
-            urls = [{
-              template = "https://search.nixos.org/packages";
-              params = [
-                { name = "type"; value = "packages"; }
-                { name = "query"; value = "{searchTerms}"; }
-              ];
-            }];
+            urls = [
+              {
+                template = "https://search.nixos.org/packages";
+                params = [
+                  {
+                    name = "type";
+                    value = "packages";
+                  }
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
             icons = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
             definedAliases = [ "@np" ];
           };
           "NixOS Options" = {
-            urls = [{
-              template = "https://search.nixos.org/options";
-              params = [
-                { name = "type"; value = "packages"; }
-                { name = "query"; value = "{searchTerms}"; }
-              ];
-            }];
+            urls = [
+              {
+                template = "https://search.nixos.org/options";
+                params = [
+                  {
+                    name = "type";
+                    value = "packages";
+                  }
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
             icons = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
             # icons = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-os.svg";  # NOTE: Try this after building
             definedAliases = [ "@no" ];
           };
           "NixOS Wiki" = {
-            urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
+            urls = [ { template = "https://nixos.wiki/index.php?search={searchTerms}"; } ];
             icon = "https://nixos.wiki/favicon.png";
             updateInterval = 1000 * 60 * 60 * 24; # daily
             definedAliases = [ "@nw" ];
           };
+          "Noogle.dev" = {
+            urls = [
+              {
+                template = "https://noogle.dev/q?term={searchTerms}";
+              }
+            ];
+            icon = "https://nixos.wiki/favicon.png";
+            updateInterval = 1000 * 60 * 60 * 24; # daily
+            definedAliases = [ "@ng" ];
+          };
           "Home Manager Options" = {
-            urls = [{
-              template = "https://home-manager-options.extranix.com/?query={searchTerms}&release=release-24.11";
-            }];
+            urls = [
+              {
+                template = "https://home-manager-options.extranix.com/?query={searchTerms}&release=release-25.05";
+              }
+            ];
             icon = "https://nixos.wiki/favicon.png";
             updateInterval = 1000 * 60 * 60 * 24; # daily
             definedAliases = [ "@ho" ];
@@ -95,6 +124,18 @@
               {
                 name = "caarlos0 dotfiles";
                 url = "https://github.com/caarlos0/dotfiles";
+              }
+              {
+                name = "nix-cheat-sheet";
+                url = "https://jdheyburn.co.uk/blog/nix-cheat-sheet";
+              }
+              {
+                name = "nix-tour";
+                url = "https://nixcloud.io/tour";
+              }
+              {
+                name = "longerhv-config";
+                url = "https://github.com/LongerHV/nixos-configuration";
               }
             ];
           }
