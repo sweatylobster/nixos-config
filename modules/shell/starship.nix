c36cc0bc14232fdfa4ce40eaad0a50ed5b8c3c57
Max de Hoyos <max.dehoyos@gmail.com>
2025-09-02 00:24:23 -0700

    fix(starship): stop disabling literally everything

--- modules/shell/starship.nix
+++ modules/shell/starship.nix
@@ -1,4 +1,5 @@
-{ ... }: {
+{ ... }:
+{
   programs.starship = {
     enable = true;
     enableTransience = true;
@@ -18,59 +19,6 @@
         ssh_symbol = "@";
         format = "$ssh_symbol[$hostname](bright-blue) ";
       };
-
-      aws.disabled = true;
-      azure.disabled = true;
-      buf.disabled = true;
-      bun.disabled = true;
-      c.disabled = true;
-      cmake.disabled = true;
-      conda.disabled = true;
-      container.disabled = true;
-      crystal.disabled = true;
-      daml.disabled = true;
-      dart.disabled = true;
-      deno.disabled = true;
-      docker_context.disabled = true;
-      dotnet.disabled = true;
-      elixir.disabled = true;
-      elm.disabled = true;
-      env_var.disabled = true;
-      erlang.disabled = true;
-      fennel.disabled = true;
-      gcloud.disabled = true;
-      golang.disabled = true;
-      gradle.disabled = true;
-      guix_shell.disabled = true;
-      haskell.disabled = true;
-      haxe.disabled = true;
-      helm.disabled = true;
-      java.disabled = true;
-      jobs.disabled = false;
-      julia.disabled = true;
-      kotlin.disabled = true;
-      kubernetes.disabled = true;
-      lua.disabled = true;
-      nim.disabled = true;
-      nix_shell.disabled = true;
-      nodejs.disabled = true;
-      ocaml.disabled = true;
-      openstack.disabled = true;
-      package.disabled = true;
-      perl.disabled = true;
-      php.disabled = true;
-      purescript.disabled = true;
-      python.disabled = true;
-      ruby.disabled = true;
-      rust.disabled = true;
-      scala.disabled = true;
-      shlvl.disabled = true;
-      singularity.disabled = true;
-      swift.disabled = true;
-      terraform.disabled = true;
-      typst.disabled = true;
-      vagrant.disabled = true;
-      zig.disabled = true;
     };
   };
 }
