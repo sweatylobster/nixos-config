8bc039717046d4e27349821e1fd4d6b358d43948
Max de Hoyos <max.dehoyos@gmail.com>
2026-05-20 23:21:35 -0700

    feat: add screencasting to niri

--- machines/shared/niri.nix
+++ machines/shared/niri.nix
@@ -1,9 +1,17 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 {
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
+    # config.common.default = "gnome";
+
+  };
   environment.systemPackages = with pkgs; [
     niri
     xwayland-satellite
     fuzzel
     swaybg
+    pipewire
+    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
   ];
 }
